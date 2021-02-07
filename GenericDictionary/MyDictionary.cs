55c2ca50539c3e800962a3150619d3168c5d8800
Fatih Kabak <fatihkabak39@hotmail.com>
2021-02-07 05:47:44 +0300

    Add project files.

---
+++ GenericDictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArray1 = keys;
+            TValue[] tempArray2 = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+                values[i] = tempArray2[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+
+        public void GetPairs()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("***********************************");
+                Console.WriteLine("Pair " + i + ": " + keys[i] + " " + values[i]);
+            }
+        }
+
+    }
+}
---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary1 = new MyDictionary<string, string>();
+            myDictionary1.Add("Test", "Test_Pair");
+            Console.WriteLine("First Dictionary;");
+            myDictionary1.GetPairs();
+            Console.WriteLine("***********************************");
+
+            MyDictionary<int, string> myDictionary2 = new MyDictionary<int, string>();
+            myDictionary2.Add(1, "Int_Test");
+            myDictionary2.Add(2, "Second_Val");
+            Console.WriteLine("\nSecond Dictionary;");
+            myDictionary2.GetPairs();
+            Console.WriteLine("***********************************");
+            Console.ReadLine();
+        }
+    }
+}
